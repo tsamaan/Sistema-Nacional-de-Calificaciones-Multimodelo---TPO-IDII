062f749f2f91c90b71ca2d7703d0843c949b4aa4
tsamaan <tsamaanpiroli@uade.edu.ar>
2026-02-19 00:55:22 -0300

    modificaciones

--- run_init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-./venv/bin/python carga/seed_edugrade.py init \
-  --mongo "mongodb://admin:admin123@localhost:27017" \
-  --redis "redis://:redis123@localhost:6379/0" \
-  --neo4j-pass "Neo4j2026!" \
-  --redis-file "db/levantar estructura en redis.txt" \
-  --neo4j-file "db/levantar colecciones en neo.txt"
